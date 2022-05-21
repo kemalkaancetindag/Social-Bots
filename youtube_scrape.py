5d61a23ba7d788640a2e0ce9412bf52f244560ba
Kemal Kaan Çetindağ <58605537+kemalkaancetindag@users.noreply.github.com>
2022-05-21 19:15:25 +0300

    youtube scores added

--- youtube_scrape.py
+++ youtube_scrape.py
@@ -37,9 +37,32 @@ def get_last_videos_stats(video_ids):
             if len(response.json()["items"]) > 0:
                 
                 video_statistics = response.json()["items"][0]["statistics"]
+                video_statistics["video_url"] = f"https://www.youtube.com/watch?v={vid}"
+                video_statistics["video_id"] = vid
                 video_stats.append(video_statistics)
     return video_stats
-                
+
+
+def calculate_engagement_followers(video_stats, total_subs):
+    total_likes = 0
+    for stat in video_stats:
+        total_likes += int(stat["likeCount"])
+
+    follower_engagement = (total_likes/int(total_subs))*100
+    return follower_engagement
+
+def calculate_popularity(video_stats,total_impression):
+    total_likes = 0
+    total_comments = 0
+    for stat in video_stats:
+        total_likes += int(stat["likeCount"])
+        total_comments += int(stat["commentCount"])
+
+    total_engagement = total_likes + total_comments
+    popularity_score = (total_engagement/int(total_impression))*100
+
+    return popularity_score
+
         
 
 channel_id = get_channel_id("newdaynewgame")
@@ -50,10 +73,15 @@ channel_stats, title = get_channel_stats(channel_id)
 
 video_stats = get_last_videos_stats(video_ids)
 
+engagement_score = calculate_engagement_followers(video_stats, channel_stats["subscriberCount"])
+popularity_score = calculate_popularity(video_stats,channel_stats["viewCount"])
+
 influencer_object = {}
 
 influencer_object["title"] = title
 influencer_object["channel_id"] = channel_id
+influencer_object["interraction_result"] = engagement_score
+influencer_object["popularity_result"] = popularity_score
 influencer_object["channel_stats"] = channel_stats
 influencer_object["last_videos"] = video_stats
 
